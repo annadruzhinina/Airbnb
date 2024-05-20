f75f88f10f8ef18157486bcb3a14f695f09613f6
anna <anna@rcbd.org>
2024-05-20 01:01:55 -0400

    Working on navbar

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -7,15 +7,7 @@ export default function Search() {
         <div className="serach-container">
           <div
           className="search-menu">
-              <div className="serach-menu-item">
-                Where <br/> <span>Search destination</span>
-              </div>
-              <div className="serach-menu-item">
-                  Check In <br/> <span>Add dates</span>
-              </div>
-              <div className="serach-menu-item">
-                  Check Out <br/> <span>Add dates</span>
-              </div>
+             
               <div className="serach-menu-item">
                   <div className="search-add">Who <br/><span>Add Guests</span></div>
                   <div className="search-icon">
---
+++ src/components/search/SearchItem.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+import './SerachItem.scss'
+
+export default function SearchItem({title, description, direction}) {
+    return (
+        <div className='searchItem-container'>
+
+        </div>
+    )
+}
+//  <div className="serach-menu-item">
+//                 Where <span>Search destination</span>
+//               </div>
+//               <div className="serach-menu-item">
+//                   Check In <span>Add dates</span>
+//               </div>
+//               <div className="serach-menu-item">
+//                   Check Out<span>Add dates</span>
+//               </div>
